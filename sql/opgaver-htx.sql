04ef9ebd89daaa348579edc4a1f399557660e6da
Mikkel K Mosthaf <mosthaf@gmail.com>
2025-02-26 04:02:25 +0100

    add htx opgaver

---
+++ sql/opgaver-htx.sql
@@ -0,0 +1,40 @@
+/******* OPGAVER ***********************************************
+Forsøg at lave disse forespørgsler (queries) for at løse opgaverne.
+Test dine forespørgsler ved at kopiere dem til vinduet "Query SQL"
+og tryk "> RUN" for at se resultaterne.
+Skriv din svar herunder hver opgave og gem i din logbog.
+*************************************************************************/
+
+/******************************************************
+Opgave 1: Hent alle produkter med en pris under 1000 kr.
+Skriv din forespørgsel herunder:
+******************************************************/
+
+/******************************************************
+Opgave 2: Find alle kunder, der har købt Adidas-produkter.
+Udskriv kun kundens navn og email og skoens model.
+Skriv din forespørgsel herunder:
+******************************************************/
+
+/******************************************************
+Opgave 3: Opdater prisen på 'Vans Old Skool' til 799.00 kr.
+Tjek at prisen er blevet opdateret ved at køre en SELECT-forespørgsel.
+Skriv din forespørgsel herunder:
+******************************************************/
+
+/******************************************************
+Opgave 4: Slet alle ordrer fra en bestemt kunde, hvis kunden 
+ikke har nogen ordrer.
+Tjek først kundens ID ved at køre en SELECT-forespørgsel.
+Tjek derefter, om kunden har ordrer i Ordre-tabellen.
+Efter at have slettet kunden, skal du køre en SELECT-forespørgsel.
+Skriv din forespørgsel herunder:
+******************************************************/
+
+/******************************************************
+Opgave 5: Tilføj en ny ordre, hvor Sofie Thomsen køber en Asics Gel-Kayano 28 (1 stk).
+Find produktets ID og kundens id ved at køre en SELECT-forespørgsel.
+Find et nyt ordre-ID ved at tælle antallet af rækker i Ordre-tabellen.
+Brug derefter disse ID'er til at indsætte en ny ordre i Ordre-tabellen.
+Skriv din forespørgsel herunder:
+******************************************************/
